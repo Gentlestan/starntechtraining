00608a74bec17c91883718ab071f88d5dc18d8ae
Gentlestan <81222819+Gentlestan@users.noreply.github.com>
2024-04-30 14:43:02 +0100

    deleted unused code block

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,7 +1,6 @@
 import InstallationImage from '/public/trainingImg.jpg'; // Import the service image
 import Hero from "@/components/Hero";
 import Image from "next/image";
-import Link from "next/link"; 
 
 export default function Services() {
     return (
@@ -179,69 +178,3 @@ export default function Services() {
 }
 
 
-
-/*import InstallationImage from "/public/installation3.avif"; // Import the service image
-import Hero from "@/components/Hero";
-import Image from "next/image";
-import Link from "next/link"; 
-
-export default function Services() {
-    return (
-        <div>
-            <Hero
-                imgData={InstallationImage}
-                imgAlt='solar Installation'
-                title="Your investment deserves expertise. Trust us to guide and execute your professional installations. From solar to CCTV, fire alarms, and smart Home amplifiers, we've got you covered"
-            />
-            <h1 className="text-2xl md:text-3xl xl:text-4xl text-center mt-4 font-bold">Our Services</h1>
-            <div>
-                <div>
-                    <h1 className="text-3xl text-center md:text-left md:ml-48 xl:ml-48 mt-4">Renewable Energy/Solar system Installation</h1>
-                </div>
-                <div className="text-center flex items-start justify-start mt-16 flex-col md:flex-col xl:flex-row">
-                    <Image 
-                        src="/inverter-1.jpeg" 
-                        alt="training image"
-                        width={500}
-                        height={500}
-                        className="mx-auto md:ml-48 xl:ml-48 xl:block"
-                    />
-                    <ul className="list-disc list-inside text-2xl text-start xl:text-3xl py-2 ml-2 md:ml-8 xl:ml-8">
-                        <li className="py-2">Complete nationwide installation of solar systems and inverters, covering solar panels, inverters, batteries, and more.</li>
-                        <li className="py-2">Segregation of inverter and non-inverter circuits, enabling you to power essential appliances using solar energy while heavier appliances can run on PHCN or generator power.</li>
-                        <li className="py-2">Implementation of earthing to safeguard your solar equipment against lightning strikes.</li>
-                        <li className="py-2">Rapid installation turnaround, with services completed within two business days in Abuja and five business days outside Abuja.</li>
-                        <li className="py-2">Our team comprises highly skilled in-house installers, with qualifications in Electrical Engineering, including both BSc and HND certifications.</li>
-                        <li className="py-2">Utilization of high-quality, cost-effective components sourced from leading manufacturers such as Felicity, Index, luminous, afripower, Schneider (Xantrex), US Battery, indian batteries, Multipower Addo Su-Kam, and Luminous. Alternatively, you have the option to supply your own components.</li>
-                        <li className="py-2">
-                            <Link href="/recent-solar-installations" legacyBehavior>
-                                <a className="text-blue-900">Click to see some of our recent solar system installations</a>
-                            </Link>
-                        </li>
-                    </ul> 
-                </div>
-                <div>
-                    <h1 className="text-3xl text-center md:text-left md:ml-48 xl:ml-48 mt-4">Security/CCTV system Installation</h1>
-                </div>
-                <div className="text-center flex items-start justify-start mt-16 flex-col md:flex-col xl:flex-row">
-                    <Image 
-                        src="/installation-cctv1.jpeg" 
-                        alt="training image"
-                        width={500}
-                        height={500}
-                        className="mx-auto md:ml-48 xl:ml-48 xl:block"
-                    />
-                    <ul className="list-disc list-inside text-2xl text-start xl:text-3xl py-2 ml-2 md:ml-8 xl:ml-8">
-                        <li className="py-2">Comprehensive CCTV installation services for residential and commercial properties</li>
-                        <li className="py-2">Customized CCTV system design tailored to your specific security needs</li>
-                        <li className="py-2">Installation of high-definition cameras for clear and detailed video surveillance.</li>
-                        <li className="py-2">Integration of advanced features such as motion detection, night vision, and remote access.</li>
-                        <li className="py-2">Professional setup of DVR/NVR systems for video recording and storage.</li>
-                        <li className="py-2">Expert placement of cameras to ensure maximum coverage and effectiveness.</li>
-                        <li className="py-2">Configuration of mobile apps for convenient remote monitoring on smartphones and tablets.</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    );
-}*/
